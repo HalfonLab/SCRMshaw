4726f0b1799e3cc67f11e9aa2f3301a1d95bc2fb
weiyangedward <weiyangedward@gmail.com>
2017-04-24 22:06:04 -0700

    init repo

---
+++ example/run_test.sh
@@ -0,0 +1,1 @@
+perl ../code/scrm.pl --thitg 300 --gff ./test_data/eve.gff3 --imm --hexmcd --pac --genome ./test_data/eve.location.fa --traindirlst ./test_data/trainingSet.lst --outdir ./test_output_1
